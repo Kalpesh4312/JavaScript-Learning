3610872c3f027574ed23e52a8e63a4515e317ec7
Kalpesh Patil <kalpeshpatil431256@gmail.com>
2025-01-28 19:18:27 +0530

    Add files via upload

---
+++ 11_lexical_scope_closure/index.js
@@ -0,0 +1,31 @@
+//***************** Lexical Scope ******************** */
+
+// function outer() {
+//     const name = 'kalpesh'
+//     function inner() {
+//         console.log(`inner : ${name}`);
+        
+//     }
+
+//     inner()
+//     console.log(`outer : ${name}`);
+// }
+
+// outer()
+
+
+
+//************* Closure ************************ */
+
+// function outer() {
+//     const name = 'kalpesh'
+//     function inner() {
+//         console.log(`inner : ${name}`);
+//     }
+//     console.log(`outer : ${name}`);
+//     return inner
+
+// }
+
+// const inner = outer()
+// inner()
